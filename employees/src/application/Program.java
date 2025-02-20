b7012e9d6a16413bee4ec6cabb6c4d1578816dad
Iago Oliveira <deviagooliveira@gmail.com>
2025-02-20 09:09:41 -0300

    exercicio de lista

--- employees/src/application/Program.java
+++ employees/src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import entities.Employees;
 import java.util.Scanner;
 
 public class Program {
@@ -8,6 +11,41 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		List<Employees> employee = new ArrayList<>();
+
+		System.out.print("How many employees will be registered? ");
+
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("\nEmplyoee #" + (i + 1) + ":");
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			double salary = sc.nextDouble();
+			employee.add(new Employees(id, name, salary));
+		}
+
+		System.out.print("\nEnter the employee id that will have salary increase : ");
+		int id = sc.nextInt();
+		Employees obj = employee.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+
+		if (obj == null) {
+			System.out.println("This id does not exist!");
+		} else {
+			System.out.print("Enter the percentage: ");
+			double percent = sc.nextDouble();
+			obj.increaseSalary(percent);
+		}
+
+		System.out.println("\nList of employees:");
+		for (Employees z : employee) {
+			System.out.println(z);
+		}
 
 		sc.close();
 
--- employees/src/entities/Employees.java
+++ employees/src/entities/Employees.java
@@ -1,6 +1,39 @@
 package entities;
 
 public class Employees {
-	
+	private int id;
+	private String name;
+	private double salary;
 
+	public Employees(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void increaseSalary (double percent) {
+		salary += (salary * percent / 100);
+	}
+	
+	public String toString() {
+		return id + ", " + name + ", " + String.format("%.2f", salary);
+	}
+	
+	
 }
